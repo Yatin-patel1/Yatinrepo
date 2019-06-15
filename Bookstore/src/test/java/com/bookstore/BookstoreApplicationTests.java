c464f43cc4935fff3a7d67581ba5f22e4787daa5
Aslan <patel.yatin9227@gmail.com>
2019-06-15 11:38:10 -0400

    Changed Test.java

--- Bookstore/src/test/java/com/bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/com/bookstore/BookstoreApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
-
+/*Test111*/
 	@Test
 	public void contextLoads() {
 	}
